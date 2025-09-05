0b1b940cded23467b6dcc5b16044fd4711977170
Shirakami_Cocoa6078 <71052778+ShirakamiCocoa6078@users.noreply.github.com>
2025-09-05 12:00:59 +0900

    레이팅 계산 시스템 일부 수정 및 곡 데이터 업데이트

--- src/lib/ratingUtils.ts
+++ src/lib/ratingUtils.ts
@@ -11,32 +11,37 @@ export function calculateRating(constant: number, score: number): number {
     return constant + 2.15;
   }
   if (score >= 1007500) { // SSS
-    return constant + 2.00 + ((score - 1007500) * 0.0001);
+    return constant + 2.00 + ( Math.floor((score - 1007500)/100) * 0.01);
   }
   if (score >= 1005000) { // SS+
-    return constant + 1.50 + ((score - 1005000) * 0.0002);
+    return constant + 1.50 + ( Math.floor((score - 1005000)/50) * 0.01);
   }
   if (score >= 1000000) { // SS
-    return constant + 1.00 + ((score - 1000000) * 0.0001);
+    return constant + 1.00 + ( Math.floor((score - 1000000)/100) * 0.01);
+  }
+  if (score >= 990000) { // S+
+    return constant + 0.6 + ( Math.floor((score - 990000)/250) * 0.01);
   }
   if (score >= 975000) { // S
     // S부터 S+ 까지는 15000점 구간에 1.00의 레이팅이 배분되어 있음.
     // 975000점: +0.0, 990000점: +0.5, 1000000점: +1.0
-    return constant + ((score - 975000) / 25000);
+    return constant + ( Math.floor((score - 975000)/250) * 0.01);
   }
   if (score >= 950000) { // AAA
-    return constant - 1.50 + ((score - 950000) * 0.00006); // (3.00 / 25000)
+    return constant - 1.50; // (3.00 / 25000)
   }
   if (score >= 925000) { // AA
-    return constant - 3.00 + ((score - 925000) * 0.00006);
+    return constant - 3.00;
   }
   if (score >= 900000) { // A
-    return constant - 5.00 + ((score - 900000) * 0.00008); // (2.00 / 25000)
+    return constant - 5.00; // (2.00 / 25000)
   }
   if (score >= 800000) { // BBB
-    const baseRating = (constant - 5.00) / 2;
-    return baseRating + (((score - 800000) / 100000) * baseRating);
+    return (constant - 5.00) / 2;
   }
   // B, C, D
+  if(score < 800000){
+    return 0;
+  }
   return 0;
 }
